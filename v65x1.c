3635251397a3ab80d49af6b5ec995f69a05a5c70
tterp <torbenterp@privat.dk>
2017-11-19 20:11:24 +0000

    Add files via upload

--- v65x1.c
+++ v65x1.c
@@ -110,7 +110,9 @@ char buffer[80];
         XFillRectangle(dpy, win, DefaultGC(dpy, screen), 29, 28, 26, 3);
         
         XFillRectangle(dpy, win, DefaultGC(dpy, screen), 29, 34, 26, 3);
-       printf("tast s\n");
+        XSetForeground(dpy, DefaultGC(dpy, screen), blue.pixel);
+        XFillArc(dpy, win, DefaultGC(dpy, screen), 10, 12, 23 , 31, 90*64, 180*64);
+        printf("tast s\n");
 
   if ((keysym == XK_s) && (event.type == KeyRelease))
 		
@@ -120,6 +122,50 @@ char buffer[80];
    }
 }
  
+ if (keysym == XK_o)
+		{
+	 XSetForeground(dpy, DefaultGC(dpy, screen), black.pixel);
+		XFillRectangle(dpy, win, DefaultGC(dpy, screen), 65, 0, 64, 53);
+	XSetForeground(dpy, DefaultGC(dpy, screen), red.pixel);
+		XFillRectangle(dpy, win, DefaultGC(dpy, screen), 64, 0, 64, 53);
+	
+		XSetForeground(dpy, DefaultGC(dpy, screen), white.pixel);
+        XFillRectangle(dpy, win, DefaultGC(dpy, screen), 96, 14, 8, 3);
+        XFillRectangle(dpy, win, DefaultGC(dpy, screen), 99, 17, 3, 2);
+        XFillRectangle(dpy, win, DefaultGC(dpy, screen), 93, 19, 24, 3);
+        XFillRectangle(dpy, win, DefaultGC(dpy, screen), 108, 22, 3, 15);
+        XFillRectangle(dpy, win, DefaultGC(dpy, screen), 89, 34, 19, 3);
+        XFillRectangle(dpy, win, DefaultGC(dpy, screen), 71, 19, 5, 3);
+        XFillRectangle(dpy, win, DefaultGC(dpy, screen), 115, 19, 3, 8);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 75, 22, 89, 36);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 76, 22, 90, 36);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 77, 22, 91, 36);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 78, 22, 92, 36);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 109, 29, 117, 24);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 109, 30, 117, 25);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 109, 31, 117, 26);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 87, 25, 93, 19);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 88, 25, 94, 19);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 89, 25, 94, 20);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 90, 25, 94, 21);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 76, 19, 90, 23);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 76, 20, 90, 24);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 76, 21, 90, 25);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 76, 22, 90, 25);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 69, 29, 69, 30);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 70, 27, 70, 31);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 71, 25, 71, 32);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 72, 27, 72, 31);
+        XDrawLine(dpy, win, DefaultGC(dpy, screen), 73, 29, 73, 30);
+        printf("tast o\n");
+
+  if ((keysym == XK_o) && (event.type == KeyRelease))
+		
+		{
+      XSetForeground(dpy, DefaultGC(dpy, screen), black.pixel);
+		XFillRectangle(dpy, win, DefaultGC(dpy, screen), 64, 0, 64, 53);
+   }
+}
   
 
 /* XComposeStatus compose; is not used, though it's in some books */
